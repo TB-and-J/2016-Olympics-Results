3c32eddbe694e27df68d8a40b46ece44b4caf8be
Huy Pham (Benjamin) <benjaminhuypham@gmail.com>
2018-11-17 13:15:10 -0700

    final project

--- Rio_Olympics_DB.sql
+++ Rio_Olympics_DB.sql
@@ -1,60 +1,41 @@
-CREATE DATABASE rio_olympics;
+CREATE DATABASE rio_olympics_db;
 
-USE rio_olympics;
+USE rio_olympics_db;
 
 CREATE TABLE country(
-	id INT PRIMARY KEY,
-    country varchar(100),
+	id INT,
+    country varchar(200),
     population INT,
-    gdp_per_capita FLOAT(10, 2)
+    gdp_per_capita FLOAT (12,2)
 );
 
 CREATE TABLE sport(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    sport_name VARCHAR(200)
+	id INT,
+    sport VARCHAR(200)
 );
 
 CREATE TABLE athlete(
-	id INT PRIMARY KEY,
+	id INT,
     name varchar(100),
     sex varchar(2),
     age INT,
     height INT,
-    weight INT,
-    country_id INT,
-    sport_id INT,
-    FOREIGN KEY (country_id)
-		REFERENCES country(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-	FOREIGN KEY (sport_id)
-		REFERENCES sport(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    weight INT
 );
 
 CREATE TABLE medal(
-    athlete_id INT,
-    country_id INT,
-    gold_count INT,
+	medal_count INT, 
+    gold_count  INT,
     silver_count INT,
-    bronze_count INT,
-    FOREIGN KEY (athlete_id)
-		REFERENCES athlete(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-	FOREIGN KEY (country_id)
-		REFERENCES country(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    bronze_count INT
 );
 
 CREATE TABLE event(
 	id INT,
-    sport_id INT,
-    event_name VARCHAR(350),
-    FOREIGN KEY (sport_id)
-		REFERENCES sport(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    sport VARCHAR(350),
+    event VARCHAR(350)
 );
+
+
+
+
---
+++ config.py
@@ -0,0 +1,4 @@
+un='root'
+pw='DenverCo123'
+port=3306
+uri='localhost'
